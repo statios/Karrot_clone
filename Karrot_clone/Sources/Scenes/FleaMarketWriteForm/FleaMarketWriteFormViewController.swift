eabd79c3d04a72a7d5413aa77d2001301b3a2d49
statios <statios@kakao.com>
2021-01-11 04:23:48 +0900

    Remove nimble framwork

--- Karrot_clone/Sources/Scenes/FleaMarketWriteForm/FleaMarketWriteFormViewController.swift
+++ Karrot_clone/Sources/Scenes/FleaMarketWriteForm/FleaMarketWriteFormViewController.swift
@@ -31,7 +31,7 @@ final class FleaMarketWriteFormViewController: BaseASViewController {
     $0.setTitleColor(.systemBlue, for: .normal)
   }
   
-  private let submitButton = UIButton().then {
+  let submitButton = UIButton().then {
     $0.setTitle(Text.submit, for: .normal)
     $0.setTitleColor(.systemBlue, for: .normal)
   }
